dee2ad45954c250a0db5503f1e0fee0dcd802707
Myo Thet <mr.myothet@gmail.com>
2023-05-30 14:10:40 +0630

    Chapter 10 Modules - Node modules module.exports and require

---
+++ chapter10--modules/my_module.js
@@ -0,0 +1,12 @@
+// Modules from Node
+
+const sum = (x, y) => x + y;
+const square = (x) => x * x;
+
+// exports.sum = sum;
+// exports.square = square;
+
+module.exports = {
+  sum,
+  square,
+};
