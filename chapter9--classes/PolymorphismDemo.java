c87a6671e7c71ce8036fd4743a92471fb22f2ce8
Myo Thet <mr.myothet@gmail.com>
2023-06-05 16:10:54 +0630

    After installing Extension Pack for Java from microsoft - Changes in java file

--- chapter9--classes/PolymorphismDemo.java
+++ chapter9--classes/PolymorphismDemo.java
@@ -1,35 +1,34 @@
-public class Shape{
-    public void draw(){
-        System.out.println('Shape draw');
+class Shape {
+    public void draw() {
+        System.out.println("Shape draw");
     }
 }
 
-public class Circle{
-    public void draw(){
-        System.out.println('Circle draw');
+class Circle extends Shape {
+    public void draw() {
+        System.out.println("Circle draw");
     }
 }
 
-public class Rectangle {
-    public void draw(){
-        System.out.println('Rectangle draw');
+class Rectangle extends Shape {
+    public void draw() {
+        System.out.println("Rectangle draw");
     }
 }
 
-public class Another {
-    public void draw(){
-        System.out.println('Rectangle draw');
+class Another {
+    public void draw() {
+        System.out.println("Rectangle draw");
     }
 }
 
-public class PolymorphismDemo
-{
-    public static void main(String[] args){
+public class PolymorphismDemo {
+    public static void main(String[] args) {
         Shape shape = new Circle();
         shape.draw();
 
         shape = new Rectangle();
         shape.draw();
-        
+
     }
 }
